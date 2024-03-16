d3b73694e3034635225e859f611d1006c4e919a0
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-16 17:33:03 -0300

    criando rotas

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def sobre(request):
+    return render(request, 'sobre.txt')
