52f9803273e33976585745f60dc0b7010526b519
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-16 17:50:12 -0300

    organização

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
-    return render(request, 'sobre.txt')
+    return render(request, 'recipes/sobre.html')
