4ef13d85ee46fe3080a7fb003d4cb6a661e1ec4f
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-17 14:15:06 -0300

    criação da página de login

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name':'Jorge Braz'
+    })
 
