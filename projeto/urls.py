3ba34be9439ba20a67fd41beb66d1c813d89aedc
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-24 08:06:21 -0300

    começo da criação do sql

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -23,6 +24,14 @@ urlpatterns = [
     path('', include('recipes.urls'))
    
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('recipes.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #Debug Toolbar#
 if settings.DEBUG:
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Tabela de categorias#
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
+
+#Tabela de receita#
+class Recipe(models.Model):
+    #Colunas da tabela#
+    #Coluna de titulo#
+    title = models.CharField(max_length=65)
+    #Coluna de descrição#
+    description = models.CharField(max_length=165)
+    #Coluna de slug#
+    slug = models.SlugField()
+    #Coluna de tempo de preparação#
+    preparation_time = models.IntegerField()
+    #Unidade de tempo da preparação#
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+            Category, on_delete=models.SET_NULL, null=True
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+    
+    def __str__(self):
+        return self.title
