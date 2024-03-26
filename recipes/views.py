c3672a125ef76a9e7a59870063c51a0311f257e4
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-25 23:43:49 -0300

    Página 404 (Página não encontrada)

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -11,8 +12,11 @@ def home(request):
     
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    if not recipes:
+        raise Http404('Página não encontrada')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title':  f'Categoria: {recipes.first().category.name}'
     })    
     
 def recipe(request, id):
