7fdcd7819f950cfeb65b96dcb3bfc7c87d2e5bf6
Jorge Luiz Silva Braz <braz_jorge@outlook.com>
2024-03-25 23:58:59 -0300

    Short cut da página 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from django.http import Http404
 from utils.recipes.factory import make_recipe
@@ -11,10 +11,8 @@ def home(request):
     })
     
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    if not recipes:
-        raise Http404('Página não encontrada')
-    return render(request, 'recipes/pages/category.html', context={
+   recipes = get_list_or_404(Recipe.objects.filter(category_id=category_id, is_published=True)).order_by('-id')
+   return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title':  f'Categoria: {recipes.first().category.name}'
     })    
